30c6ebcac6f43c1516abe59730e3fd69dcbb26cf
Savwendt <sa.wendt5@gmail.com>
2022-08-12 22:13:19 +1000

    Nav js

---
+++ resources/js/scripts.js
@@ -0,0 +1,15 @@
+function openNav () {
+
+let toggler = document.querySelector
+('.nav-promt');
+letnavigation =document.querySelector ('.main-nav');
+
+toggler.onclick = function () {
+
+    Navigation.classlist.toggle('open');
+}
+
+}
+
+openNav();
+
